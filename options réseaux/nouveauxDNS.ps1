96a81dbcb178bcf1b419f33891d13e292fdcf5f9
jpitault <40543196+jpitault@users.noreply.github.com>
2018-07-03 09:22:19 +0200

    dossier option réseaux

---
+++ options réseaux/nouveauxDNS.ps1
@@ -0,0 +1,6 @@
+﻿# changer le DNS
+
+$Adressedns = Read-Host -Prompt 'Le nouveaux DNS'
+
+
+netsh int ip set dns "LanInt" static $Adressedns primary
---
+++ options réseaux/rentreraddip.ps1
@@ -0,0 +1,11 @@
+﻿
+# Changer l'adresse IP de LanInt
+$Adresseip = Read-Host -Prompt 'Nouvelle adresse IP'
+
+$Mask = Read-Host -Prompt 'Le masque de sous réseau'
+
+$Gateway = Read-Host -Prompt 'La passerelle'
+
+Write-Host "L'adresse IP est '$Adresseip' , le masque '$Mask' , la gateway '$Gateway' "
+
+netsh int ip set address "LanInt" static $Adresseip $Mask $Gateway 1 
