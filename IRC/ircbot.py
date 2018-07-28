37472c6a3d1831fda89312d154a507db0ad93d72
jpitault <40543196+jpitault@users.noreply.github.com>
2018-07-28 22:12:19 +0200

    Update ircbot.py

--- IRC/ircbot.py
+++ IRC/ircbot.py
@@ -45,4 +45,45 @@ def sendmsg(msg, target=channel):
 def main():
     joinchan(channel)
     while 1:
+        # On prend l'info que le serveur irc nous envoie (en bytes), on le met dans "ircmsg" et on enlève le retour à la ligne.
+        ircmsg = ircsock.recv(2048).decode("UTF-8")
+        ircmsg = ircmsg.strip('\n\r')
+        print(ircmsg)
+
+        # On regarde si dans ce message il y a PRIVMSG. Dans IRC, un message se trouve sous la forme : ":[Nick]!~[hostname]@[IP Address]PRIVMSG[channel]:[message]" donc on sépare chacun des éléments dans une variable.
+        if ircmsg.find("PRIVMSG") != -1:
+            name = ircmsg.split('!',1)[0][1:]
+            message = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
+
+            # On commence par vérifié que le nom de celui qui a envoyé le message est inférieur à 17 caractères
+            if len(name) < 17:
+                # Ensuite si, dans le message on trouve une salutation pour le bot, on répond dans le général.
+                if message.find('Hi ' + botnick) != -1:
+                    sendmsg("Hello " + name + "!")
+
+                # On regarde aussi si il y a une commande dans le message
+                if message[:5].find('.tell') != -1:
+                    target = message.split(' ', 1)[1]
+                    if target.find(' ') != -1:
+                        message = target.split(' ', 1)[1]
+                        target = target.split(' ')[0]
+                    else:
+                        target = name
+                        message = "Could not parse. The message should be in the format of '.tell [target] [message]' to work properly."
+                    sendmsg(message, target)
+
+                # Maintenant on va regarder si le bot a vu un message d'arrêt de la part de l'admin
+                if name.lower() == adminname.lower() and message.rstrip() == exitcode:
+                    sendmsg("oh...okay. :'(")
+                    ircsock.send(bytes("QUIT \n", "UTF-8"))
+                    return
+
+            # Si le message n'est pas un PRIVMSG, ça peut quand même être un ping, donc on répond.
+        else:
+            if ircmsg.find("PING :") != -1:
+                ping()
+
+# On appel la fonction main
+main()
+
         
