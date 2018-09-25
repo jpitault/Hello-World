dfd8a1ade87ae40f1ffa47f50cab068cbe3ec94d
jpitault <40543196+jpitault@users.noreply.github.com>
2018-09-25 16:19:37 +0200

    Update menage.sh

--- conf_dhcp/menage.sh
+++ conf_dhcp/menage.sh
@@ -8,7 +8,7 @@ read cible
 
 # Supprimer le fichier qui contient la config host
 fichier="/etc/dhcp/$cible"
-rm fichier
+rm "$fichier"
 
 # Supprimer la ligne qui include le fichier host dans le fichier /etc/dhcp/dhcpd.conf
 grep -v "$cible" /etc/dhcp/dhcpd.conf > temp && mv temp /etc/dhcp/dhcpd.conf
